f8d158c7f431ab5ce1e8a82c262e1730cd188a7e
Luis Robles <luisgeremiasrobles@gmail.com>
2025-10-29 20:32:13 -0300

    Se agrego la funcion clearErrors de react hook form para limpiar los errores al escribir en el formulario

--- src/components/FiltroColores.jsx
+++ src/components/FiltroColores.jsx
@@ -4,24 +4,36 @@ import Form from 'react-bootstrap/Form'
 import { useForm } from 'react-hook-form';
 
 const FiltroColores = () => {
-    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const { register, handleSubmit, formState: { errors }, reset, clearErrors } = useForm();
 
     const postValidaciones = (data) => {
         console.log(data);
+        reset();
     }
 
     return (
         <section className='container'>
             <Form onSubmit={handleSubmit(postValidaciones)}>
                 <Form.Group className="d-flex justify-content-between">
-                    <Form.Control type="text" placeholder="ingrese el nombre o el codigo del color a filtrar" />
-                    <Form.Text className="text-danger d-none">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+                    <Form.Control type="text" placeholder="ingrese el nombre del color a filtrar" {...register("nombreColor", {
+                        required: "Este campo es obligatorio",
+                        minLength: {
+                            value: 3,
+                            message: "El nombre del color debe contener minimo 3 caracteres"
+                        },
+                        maxLength: {
+                            value: 12,
+                            message: "El nombre del color debe contener maximo 12 caracteres"
+                        }
+                    })}
+                        onChange={() => clearErrors("nombreColor")} />
                     <Button variant="success" type="submit" className='ms-1'>
                         Buscar
                     </Button>
                 </Form.Group>
+                <Form.Text className="text-danger">
+                    {errors.nombreColor?.message}
+                </Form.Text>
             </Form>
 
         </section>
